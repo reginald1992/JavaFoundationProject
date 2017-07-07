5379de35faa7ec19e3bb9dfd9b6a87885eaed743
Shulin <shulinliu@aliyun.com>
2017-07-07 09:39:33 +0800

    JavaSE 尚硅谷课程笔记 第三章 面向对象的编程 建立一个新的数组类，实现求最大值、最小值、排序等功能

---
+++ src/ArrayUtil.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Shulin Liu on 2017/7/7 0007.
+ */
+
+public class ArrayUtil {
+//* 求数组的最大值
+    public int getMax(int[] arr){
+        return 0;
+    }
+//* 求数组的最小值
+    public int getMin(int[] arr){
+        return 0;
+    }
+//* 遍历数组元素
+    public void printArray(int[] arr){
+
+    }
+//* 求数组的平均数
+    public int avg(int[] arr){
+        return 0;
+    }
+//* 求数组的总和
+    public int getSum(int[] arr){
+        return 0;
+    }
+//* 数组的反转
+    public int[] reverse(int[] arr){
+        return null;
+    }
+//* 实现数组的复制
+    public int[] copy(int[] arr){
+        return null;
+    }
+//* 对数组进行排序
+    public void sort(int[] arr){
+
+    }
+}
