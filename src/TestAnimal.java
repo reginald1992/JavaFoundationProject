39298aa6a41fa119eaa3f7bafeed92ce31f31b4c
Shulin <shulinliu@aliyun.com>
2017-08-13 14:41:13 +0800

    第三章 面向对象的编程 面向对象的特征：封装与隐藏 权限修饰符

--- src/TestAnimal.java
+++ src/TestAnimal.java
@@ -1,11 +1,18 @@
 /**
-* 面向对象的特征一：封装与隐藏
+* 一、面向对象的特征一：封装与隐藏
  * 1、当创建了类的对象以后，如果直接通过“对象.属性”的方式对相应的对象属性赋值的话，可能会出现不符合实际情况的意外，
  * 我们考虑不让对象来直接作用属性，而是通过“对象.方法”的形式，来控制对象对属性的访问。实际情况中，对属性的要求就通过
  * 方法来体现。
 *
- * 解决的方法：（封装的思想）将类的属性私有化，提供公共的方法（setter & getter）来实现调用
+ * 解决的方法：①（封装的思想）将类的属性私有化；②提供公共的方法（setter & getter）来实现调用
 *
+ * 二、权限修饰符：public private 缺省（即default） protected
+ *      可以用来修饰属性、方法
+ *      注意：权限从小到大：private、缺省、protected、public
+ *      对于class来说，只有public和缺省可以修饰
+ *      default类只可以被同一个包内部的类进行调用
+ *      public可以在任何地方调用
+ *
 * */
 public class TestAnimal {
     public static void main(String[] args) {
@@ -15,11 +22,11 @@ public class TestAnimal {
         a2.eat();
         a2.info();
         a2.sleep();
-        a2.setLegs(4);
+        a2.setLegs(3);
     }
 }
 class Animal2 {
-//    private 修饰的属性，只能在本类中被调用，出了次类就不能再被调用
+//    private 修饰的属性，只能在本类中被调用，出了此类就不能再被调用
     private String name;//动物的名字
     private int legs;//腿的个数
 
