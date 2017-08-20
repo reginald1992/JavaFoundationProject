f7bfc089c49207c492d2b240d0f4bb00e87a9c11
Shulin <shulinliu@aliyun.com>
2017-08-20 20:59:53 +0800

    第三章 面向对象的编程 package、import的使用

---
+++ src/TestPackageImport.java
@@ -0,0 +1,16 @@
+/**
+ * package:声明源文件所在的包，写在程序的第一行
+ *每“.”一次，表示一层文件目录
+ * 包名都要小写
+ *
+ *import:（1）显示导入指定包下的类或接口
+ *       （2）写在包的声明和源文件之间
+ *       （3）如果需要引入多个类或接口，那么就并列写出
+ *       （4）如果导入的类是在java.lang包下，如System/String/Math等，就不需要显式的声明
+ *       （5）理解.*的概念，例如java.util.*
+ *       （6）如何处理同名类的导入。如：在util包和sql包下同时存在Date类
+ *       （7）import static 表示导入指定类的static的属性或方法
+ *       （8）导入java.lang.*只能导入lang包下的所有类或接口，不能导入lang的子包下的类或结果
+ * */
+public class TestPackageImport {
+}
