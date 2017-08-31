cb19aaa5e706065cc572a41878752ca8cb68deb5
Shulin <shulinliu@aliyun.com>
2017-08-31 20:09:29 +0800

    第四章 高级类特性 继承

---
+++ src/Chapter4/TestExtends.java
@@ -0,0 +1,10 @@
+package Chapter4;
+
+public class TestExtends {
+    public static void main(String[] args) {
+        Student s=new Student();
+        s.eat();
+        Worker w=new Worker();
+        w.eat();
+    }
+}
